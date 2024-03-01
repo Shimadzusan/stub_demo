200c59d385e39443367aabadc25ef507a3596337
worker <nix_olympica@tut.by>
2024-03-01 11:07:00 +0300

    added maven

--- src/main/java/core/Launch.java
+++ src/main/java/core/Launch.java
@@ -1,8 +1,15 @@
 package core;
 
+import org.json.JSONObject;
+
 public class Launch {
     public static void main(String[] args) {
         System.out.println("..stub_demo start");
+
+        String data = "{\"number_1\": 700, \"number_2\": 200}";
+        JSONObject jsonObject = new JSONObject(data);
+        int result = jsonObject.getInt("number_1");
+        System.out.println(result);
 
         String valueFromCmd = "";
         if (args.length > 0) {
@@ -14,5 +21,9 @@ public class Launch {
         NumberSquare numberSquare = new NumberSquare();
         int number = numberSquare.returnNumberSquare(Integer.parseInt(valueFromCmd));
         System.out.println("the square of the number is: " + number);
+    }
+
+    public static String parseJson() {
+        return "";
     }
 }
