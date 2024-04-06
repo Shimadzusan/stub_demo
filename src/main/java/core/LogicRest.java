7481beb8cd383b4754b43d1b7bef01b68b7443f3
worker <nix_olympica@tut.by>
2024-04-06 07:59:11 +0300

    review_06042024

--- src/main/java/core/LogicRest.java
+++ src/main/java/core/LogicRest.java
@@ -1,11 +1,13 @@
 package core;
 
 import org.json.JSONObject;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class LogicRest {
     @PostMapping("/sendNumber")
     public String sendNumber(@RequestBody String requestBody) {
